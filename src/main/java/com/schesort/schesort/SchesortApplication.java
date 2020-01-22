40214d87f78fd3bbece3b4c86eba0fd763d2cc4d
ItsRicmor <ricardojms1999@gmail.com>
2020-01-21 20:33:54 -0600

    first commit

---
+++ src/main/java/com/schesort/schesort/SchesortApplication.java
@@ -0,0 +1,13 @@
+package com.schesort.schesort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchesortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchesortApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schesort/schesort/SchesortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schesort.schesort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchesortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
